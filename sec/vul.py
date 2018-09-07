01d6da79e101e385289064398d09444696732e10
yang.yu01 <yang.yu01@chaitin.com>
2018-09-07 19:22:58 +0800

    增加了review功能

--- sec/vul.py
+++ sec/vul.py
@@ -27,6 +27,43 @@ def vul_add(request):
 @login_required
 def vul_review(request):
     context = {}
-    vuls = VulRecord.objects.filter(vul_review=False)
+    if request.GET.get('id'):
+        if request.user.is_staff:
+            vul = VulRecord.objects.get(id=request.GET.get('id'))
+            context['vul'] = vul
+            vuls_like = VulRecord.objects.filter(vul_url__contains=vul.vul_url)
+            context['vuls_like'] = vuls_like
+            return render(request, 'one_vul.html', context)
+        else:
+            context = {
+                'message': "只有管理员才能够访问",
+                'url': "",
+                'code': "history.go(-1);"
+            }
+            return render(request, 'error.html', context)
+    else:
+        vuls = VulRecord.objects.filter(vul_review=False)
+        context['vuls'] = vuls
+        return render(request, 'vul_review.html', context)
+
+@login_required
+def vul_reviewed(request):
+    context = {}
+    if request.POST.get('id'):
+        if request.user.is_staff:
+            id = int(request.POST.get('id'))
+            vul = VulRecord.objects.get(id=id)
+            vul.vul_review = True
+            vul.vul_review_people = request.user
+            vul.vul_score = request.POST.get('score')
+            vul.save()
+        else:
+            context = {
+                'message': "只有管理员才能够访问",
+                'url': "",
+                'code': "history.go(-1);"
+            }
+            return render(request, 'error.html', context)
+    vuls = VulRecord.objects.filter(vul_review=True)
     context['vuls'] = vuls
     return render(request, 'vul_review.html', context)
