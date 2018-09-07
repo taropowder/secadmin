6d44425631e7da6db60dafb1629391c41b10385b
yang.yu01 <yang.yu01@chaitin.com>
2018-09-07 14:41:39 +0800

    fix style

--- sec/views.py
+++ sec/views.py
@@ -1,35 +1,39 @@
 # -*- coding: utf-8 -*-
 import datetime
-from django.shortcuts import render,HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Blog,CTF_learning,ON_DUTY,Book
+from .models import Blog, CTF_learning, ON_DUTY, Book
 from random import choice
-import time,datetime
+import time, datetime
+
+
 # Create your views here.
 def home(request):
     context = {}
-    weeks=Blog.objects.values('week')
+    weeks = Blog.objects.values('week')
     weekblogs = {}
     if weeks:
-        now=weeks.last()['week']
-        for w in range(1,now+1):
-            weekblogs[str(w)]=[]
-            blog=Blog.objects.filter(week=w)
+        now = weeks.last()['week']
+        for w in range(1, now + 1):
+            weekblogs[str(w)] = []
+            blog = Blog.objects.filter(week=w)
             # print blog.count()
-            if blog.count()<=5:
-                m=blog.count()
+            if blog.count() <= 5:
+                m = blog.count()
             else:
-                m=5
-            for b in range(0,m):
+                m = 5
+            for b in range(0, m):
                 # print str(w)
                 # print weekblogs
                 weekblogs[str(w)].append(blog[b])
-    a = sorted(weekblogs.items(),key=lambda d:d[0])
+    a = sorted(weekblogs.items(), key=lambda d: d[0])
     context['weekblogs'] = a
-    return render(request, 'index.html',context)
+    return render(request, 'index.html', context)
+
+
 @login_required
 def submit(request):
     context = {}
@@ -37,21 +41,23 @@ def submit(request):
     if request.method == 'POST':
         content = request.POST.get('content')
         url = request.POST.get('url')
-        direction=request.POST.get('direction')
+        direction = request.POST.get('direction')
         weeks = datetime.datetime.now().isocalendar()
-        week=weeks[1]-11
+        week = weeks[1] - 11
         blog = Blog()
-        blog.blog_user=User.objects.get(username=request.session['username'])
-        blog.content=content
-        blog.url=url
-        blog.direction=direction
-        blog.time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        blog.week=week
+        blog.blog_user = User.objects.get(username=request.session['username'])
+        blog.content = content
+        blog.url = url
+        blog.direction = direction
+        blog.time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        blog.week = week
         blog.save()
         context['statu'] = '1'
         context['error'] = "提交成功"
-        return HttpResponseRedirect('/blog/'+str(week)+'/')
-    return render(request, 'submit.html',context)
+        return HttpResponseRedirect('/blog/' + str(week) + '/')
+    return render(request, 'submit.html', context)
+
+
 def user_login(request):
     context = {}
     context['statu'] = '0'
@@ -69,82 +75,98 @@ def user_login(request):
             else:
                 context['statu'] = '1'
                 context['error'] = "您的用户已经被限制,请联系工作人员"
-                # print('密码正确，但是用户无法登录')
         else:
             context['statu'] = '1'
             context['error'] = "用户名或者密码错误"
     return render(request, 'login.html', context)
+
+
 def register(request):
     context = {}
-    context['statu']=0
+    context['statu'] = 0
     if request.method == 'POST':
         name = request.POST.get('name')
-        u=User.objects.filter(username=name)
+        u = User.objects.filter(username=name)
         if u:
             context['statu'] = 1
             context['error'] = '该名字已被使用'
             return render(request, 'register.html', context)
         password = request.POST.get('password')
         email = request.POST.get('eamil')
-        user = User.objects.create_user(name,email, password)
-        user.first_name=request.POST.get('firstname')
+        user = User.objects.create_user(name, email, password)
+        user.first_name = request.POST.get('firstname')
         user.save()
         context['name'] = name
         return render(request, 'login.html', context)
     return render(request, 'register.html', context)
-def weekblog(request,week):
+
+
+def weekblog(request, week):
     context = {}
-    blogs=Blog.objects.filter(week=week)
-    context['blogs']=blogs
+    blogs = Blog.objects.filter(week=week)
+    context['blogs'] = blogs
     context['week'] = week
     u = User.objects.values('id')
-    man=choice(u)
+    man = choice(u)
     context['man'] = man['id']
     return render(request, 'weekblog.html', context)
-def myblog(request,user_id):
+
+
+def myblog(request, user_id):
     context = {}
     blogs = Blog.objects.filter(blog_user_id=user_id)
     context['blogs'] = blogs
     return render(request, 'myblog.html', context)
+
+
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+
 def ctflerning(request):
     context = {}
     context['ctf'] = CTF_learning.objects.order_by('type')
     return render(request, 'ctflerning.html', context)
 
+
 def changeblog(request):
     context = {}
-    user_id =request.session['id']
+    user_id = request.session['id']
     blogs = Blog.objects.filter(blog_user_id=user_id)
     context['blogs'] = blogs
     return render(request, 'changemyblog.html', context)
 
-def change(request,change_id):
+
+def change(request, change_id):
     if request.method == 'POST':
         blog = Blog.objects.filter(id=change_id)
         if blog.values('blog_user_id')[0]['blog_user_id'] == request.session['id']:
             content = request.POST.get('content')
             url = request.POST.get('url')
-            Blog.objects.filter(id=change_id).update(content=content,url=url)
-            return HttpResponseRedirect('/myblog/'+str(request.session['id'])+'/')
+            Blog.objects.filter(id=change_id).update(content=content, url=url)
+            return HttpResponseRedirect('/myblog/' + str(request.session['id']) + '/')
     return HttpResponseRedirect('/changeblog/')
+
 
 def search(request):
     context = {}
     if request.method == 'POST':
         name = request.POST.get('name')
-        blogs=Blog.objects.filter(content__contains=name)
+        blogs = Blog.objects.filter(content__contains=name)
         context['blogs'] = blogs
     return render(request, 'search.html', context)
+
+
 def classification(request):
     context = {}
     if request.method == 'POST':
         direction = request.POST.get('direction')
-        blogs=Blog.objects.filter(direction=direction)
+        blogs = Blog.objects.filter(direction=direction)
         context['blogs'] = blogs
     return render(request, 'classification.html', context)
+
+
 def onduty(request):
     context = {}
     WEEK = ['Mon', 'Tue', 'Wed', 'Tur', 'Fri', 'Sat', 'Sun']
@@ -156,15 +178,16 @@ def onduty(request):
             name = request.POST.get('name')
         if name != '':
             for y in WEEK:
-                for x in range(1,7):
-                    weektime = y+str(x)
+                for x in range(1, 7):
+                    weektime = y + str(x)
                     # print request.POST.get(weektime)
                     if request.POST.get(weektime) == 'on':
                         # print weektime
                         oldpeople = ON_DUTY.objects.filter(time=weektime)
                         if oldpeople:
-                            if  name not in oldpeople[0].name:
-                                ON_DUTY.objects.update_or_create(time=weektime, defaults={'name':name+','+oldpeople[0].name})
+                            if name not in oldpeople[0].name:
+                                ON_DUTY.objects.update_or_create(time=weektime,
+                                                                 defaults={'name': name + ',' + oldpeople[0].name})
                         else:
                             ON_DUTY.objects.update_or_create(time=weektime, defaults={'name': name})
     dutys = {}
@@ -172,7 +195,7 @@ def onduty(request):
         dutys[y] = []
         for x in range(1, 6):
             weektime = y + str(x)
-            people=ON_DUTY.objects.filter(time = weektime)
+            people = ON_DUTY.objects.filter(time=weektime)
             if people:
                 # print people[0].name
                 dutys[y].append(people[0].name)
@@ -184,21 +207,28 @@ def onduty(request):
     duty['Mon'] = dutys['Mon']
     context['duty'] = dutys
     context['conut'] = WEEK
-        # print request.POST.get('Mon1')
+    # print request.POST.get('Mon1')
     return render(request, 'onduty.html', context)
+
+
 def book(request):
     context = {}
     context['books'] = Book.objects.all()
     return render(request, 'book.html', context)
+
+
 @login_required
 def lend(request):
     book = Book.objects.get(id=request.GET.get('id'))
-    if book.is_lend==False:
-        Book.objects.filter(id=request.GET.get('id')).update(lend_people_id=request.session['id'],lend_time=datetime.datetime.now(),is_lend=True)
+    if book.is_lend == False:
+        Book.objects.filter(id=request.GET.get('id')).update(lend_people_id=request.session['id'],
+                                                             lend_time=datetime.datetime.now(), is_lend=True)
     return HttpResponseRedirect('/book/')
+
+
 @login_required
 def back(request):
     book = Book.objects.get(id=request.GET.get('id'))
-    if request.session['id']== book.lend_people_id:
-        Book.objects.filter(id=request.GET.get('id')).update(lend_people_id=None,lend_time=None,is_lend=False)
+    if request.session['id'] == book.lend_people_id:
+        Book.objects.filter(id=request.GET.get('id')).update(lend_people_id=None, lend_time=None, is_lend=False)
     return HttpResponseRedirect('/book/')
