c0e808f3a2c68b3ee7a926a1f39650fea5654ff9
yang.yu01 <yang.yu01@chaitin.com>
2018-10-29 20:42:28 +0800

    增加了大数据的任务功能

--- sec/admin.py
+++ sec/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Blog, CTF_learning, ON_DUTY, Book, VulRecord, WeekLearn, WeekTask
 from .models import BlogDirection, UserProfile
+from django.utils.translation import ugettext_lazy as _
+
+
+class TaskListFilter(admin.SimpleListFilter):
+    title = _(u'提交方向')
+    parameter_name = 'direction'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('dat', _(u'大数据')),
+            ('sec', _(u'网络安全')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'sec':
+            return queryset.filter(learn_task__task_direction='sec')
+        if self.value() == 'dat':
+            return queryset.filter(learn_task__task_direction='dat')
+
+
+
+
 # Register your models here.
 admin.site.register(Blog)
 admin.site.register(CTF_learning)
@@ -25,12 +47,16 @@ class VulRecordAdmin(admin.ModelAdmin):
 
 @admin.register(WeekLearn)
 class WeekLearnAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'learn_time', 'learn_week')
+    list_display = ('id', 'name', 'learn_time', 'learn_task', 'direction_task')
     list_display_links = ('id', 'name')
-    list_filter = ('learn_week', 'learn_time')
+    list_filter = (TaskListFilter, 'learn_task')
 
     def name(self, obj):
         return str(obj.learner) + "：" + str(obj.learner.first_name)
+
+    def direction_task(self, obj):
+        # return str(obj.learn_task.get_task_direction_display())
+        return str(obj.learn_task)
 
 
 @admin.register(UserProfile)
